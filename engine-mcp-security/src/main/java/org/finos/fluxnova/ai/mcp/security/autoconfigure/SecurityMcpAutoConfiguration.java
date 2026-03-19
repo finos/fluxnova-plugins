eba753b5daeb0e556223e3e0b6beda6e83fb493f
bwright-scottlogic <bwright@scottlogic.com>
2026-03-19 16:25:38 +0000

    fix(mcp): Early Configuration
    - Add AutoConfigureAfter tag to stop configuration being done before Fluxnova's.
    
    Signed-off-by: bwright-scottlogic <bwright@scottlogic.com>

--- engine-mcp-security/src/main/java/org/finos/fluxnova/ai/mcp/security/autoconfigure/SecurityMcpAutoConfiguration.java
+++ engine-mcp-security/src/main/java/org/finos/fluxnova/ai/mcp/security/autoconfigure/SecurityMcpAutoConfiguration.java
@@ -1,9 +1,19 @@
 package org.finos.fluxnova.ai.mcp.security.autoconfigure;
 
+import org.finos.fluxnova.ai.mcp.security.permissions.McpSecurityEnginePlugin;
+import org.finos.fluxnova.ai.mcp.security.securityconfigs.SecurityConfig;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@ComponentScan(basePackages = "org.finos.fluxnova.ai.mcp.security")
+@AutoConfigureAfter(name = "org.finos.fluxnova.bpm.spring.boot.starter.FluxnovaBpmAutoConfiguration")
+@Import(SecurityConfig.class)
 public class SecurityMcpAutoConfiguration {
+
+    @Bean
+    public McpSecurityEnginePlugin mcpSecurityEnginePlugin() {
+        return new McpSecurityEnginePlugin();
+    }
 }
